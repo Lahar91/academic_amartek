8948c0d9fd00fdd76b6025921f5d7a275f499a5d
lahar91 <androrif28@gmail.com>
2023-04-05 15:28:07 +0700

    first commit

---
+++ Back_End/amartek/src/test/java/com/academic/amartek/AmartekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academic.amartek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmartekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
