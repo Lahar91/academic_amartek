7149c1a8e0d3d19459cabfbcdcab03d0b6b24ff8
Your Name <you@example.com>
2023-04-06 11:19:56 +0700

    Add Model
    add model users, role and project

---
+++ Back_End/amartek/src/main/java/com/academic/amartek/models/Role.java
@@ -0,0 +1,38 @@
+package com.academic.amartek.models;
+// package com.academic.amartek.models.user_management;
+
+// import javax.persistence.GenerationType;
+// import javax.persistence.*;
+
+// @Entity
+// @Table(name = "tb_m_role")
+// public class Role {
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Integer id;
+
+//     @Column(name = "name")
+//     private String name;
+
+//     @OneToMany(mappedBy = "Role")
+//     public Set<Users> users;
+
+//     public Integer getId() {
+//         return id;
+//     }
+//     public void setId(Integer id) {
+//         this.id = id;
+//     }
+//     public String getName() {
+//         return name;
+//     }
+//     public void setName(String name) {
+//         this.name = name;
+//     }
+//     public Set<User> getUsers() {
+//         return users;
+//     }
+//     public void setUsers(Set<User> users) {
+//         this.users = users;
+//     }
+// }
