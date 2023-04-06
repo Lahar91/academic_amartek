61c7d8dd24fca1f853048f9943458655ad812488
Rifki Rahardjo <androrif28@gmail.com>
2023-04-06 11:35:03 +0700

    marge rate to main
    marge rate to main
    fix conflig

--- Back_End/amartek/src/main/java/com/academic/amartek/models/Role.java
+++ Back_End/amartek/src/main/java/com/academic/amartek/models/Role.java
@@ -1,38 +1,40 @@
 package com.academic.amartek.models;
-// package com.academic.amartek.models.user_management;
 
-// import javax.persistence.GenerationType;
-// import javax.persistence.*;
+import javax.persistence.GenerationType;
 
-// @Entity
-// @Table(name = "tb_m_role")
-// public class Role {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Integer id;
+import java.util.Set;
 
-//     @Column(name = "name")
-//     private String name;
+import javax.persistence.*;
 
-//     @OneToMany(mappedBy = "Role")
-//     public Set<Users> users;
+@Entity
+@Table(name = "tb_m_role")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-//     public Integer getId() {
-//         return id;
-//     }
-//     public void setId(Integer id) {
-//         this.id = id;
-//     }
-//     public String getName() {
-//         return name;
-//     }
-//     public void setName(String name) {
-//         this.name = name;
-//     }
-//     public Set<User> getUsers() {
-//         return users;
-//     }
-//     public void setUsers(Set<User> users) {
-//         this.users = users;
-//     }
-// }
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "Role")
+    public Set<Users> users;
+
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Set<Users> getUsers() {
+        return users;
+    }
+    public void setUsers(Set<Users> users) {
+        this.users = users;
+    }
+}
