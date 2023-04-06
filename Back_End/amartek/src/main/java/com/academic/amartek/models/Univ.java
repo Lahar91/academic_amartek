e84f81f55369bfbfd17f4edc74bd3e869ee42db2
Ilham Buono Putra <ilhambuono@gmail.com>
2023-04-06 10:50:27 +0700

    Add Model Table tr Education, m degree, m univ, m_major
    Model For Table

---
+++ Back_End/amartek/src/main/java/com/academic/amartek/models/Univ.java
@@ -0,0 +1,50 @@
+package com.academic.amartek.models;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_m_univ")
+public class Univ {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(name = "univ_name")
+    private String univName;
+
+    @OneToMany(mappedBy = "univ")
+    private Set<Education> education;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUnivName() {
+        return univName;
+    }
+
+    public void setUnivName(String univName) {
+        this.univName = univName;
+    }
+
+    public Set<Education> getEducation() {
+        return education;
+    }
+
+    public void setEducation(Set<Education> education) {
+        this.education = education;
+    }
+}
