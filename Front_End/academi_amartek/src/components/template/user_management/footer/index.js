83a5c9b4136138a9223af469fb27bf060a447edb
Abdurrahman <Abdrhmn.gh@gmail.com>
2023-04-08 14:48:30 +0700

    add responsive for footer section

--- Front_End/academi_amartek/src/components/template/user_management/footer/index.js
+++ Front_End/academi_amartek/src/components/template/user_management/footer/index.js
@@ -1,6 +1,6 @@
 import "./index.css";
 import { NavLink } from "react-router-dom";
-import { FaRegAddressCard } from "react-icons/fa";
+import { FaRegAddressBook } from "react-icons/fa";
 import { BsTelephone, BsLink45Deg } from "react-icons/bs";
 import { AiOutlineInstagram, AiFillLinkedin } from "react-icons/ai";
 
@@ -8,22 +8,29 @@ function Footer() {
 	return (
 		<div className="wrap-foot">
 			<section className="footer">
+				{/* image footer */}
 				<div className="foot-img">
 					<img src="/amartek-logo.png" alt="Logo App" />
 				</div>
 				<div className="flex-foot-info">
+					{/* grouping footer info */}
 					<div className="foot-info">
+						{/* alamat */}
 						<div className="alamat">
-							<FaRegAddressCard size={28} color="#707B8E" />
+							<FaRegAddressBook size={23} color="#707B8E" />
 							<p>
 								Chase Plaza, Jl. Jenderal Sudirman No.Kav. 21, Kuningan, Karet, Kecamatan
 								Setiabudi, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12920
 							</p>
 						</div>
+
+						{/* nomor telepon */}
 						<div className="no-telp">
 							<BsTelephone color="#707B8E" />
 							<p>021-25989780</p>
 						</div>
+
+						{/* email */}
 						<div className="email">
 							<BsLink45Deg color="#707B8E" size={18} />
 							<NavLink to="https://www.amartek.id/" target="_blank">
@@ -31,6 +38,8 @@ function Footer() {
 							</NavLink>
 						</div>
 					</div>
+
+					{/* footer navbar section */}
 					<div className="foot-nav">
 						<ul>
 							<li>
@@ -42,6 +51,8 @@ function Footer() {
 						</ul>
 					</div>
 				</div>
+
+				{/* footer social media section */}
 				<div className="social-media">
 					<NavLink
 						to="https://www.instagram.com/amartha.teknologi/"
@@ -58,6 +69,8 @@ function Footer() {
 						<AiFillLinkedin size={20} />
 					</NavLink>
 				</div>
+
+				{/* copyright text */}
 				<p className="copyright">&copy; Created by AMARTEK GDP Bootcamp-21 🐱‍🏍</p>
 			</section>
 		</div>
