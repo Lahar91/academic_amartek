8255d5265cca4344fb2b9c90af49ea3aca75f182
lahar91 <androrif28@gmail.com>
2023-04-10 10:02:28 +0700

    fix error config email
    fix error config email

--- Back_End/amartek/src/main/java/com/academic/amartek/config/AppSecurityConfig.java
+++ Back_End/amartek/src/main/java/com/academic/amartek/config/AppSecurityConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+
 
 @Configuration
 public class AppSecurityConfig {
