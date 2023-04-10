a380f776d76b0fb761c4360693f5bf93f9e331b1
Your Name <you@example.com>
2023-04-11 04:47:20 +0700

    Refactor code , add DTO Login , Register, add JWT

---
+++ Back_End/amartek/src/main/java/com/academic/amartek/services/UserService.java
@@ -0,0 +1,17 @@
+package com.academic.amartek.services;
+
+import com.academic.amartek.models.User;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+
+public interface UserService {
+    List<User> getAll();
+    User getById(String id);
+    Boolean save(User user);
+    Boolean delete(String id);
+
+    ResponseEntity<Object> getUserById(String id);
+    List<User> getAllUsers();
+}
